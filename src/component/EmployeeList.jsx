98970ac6b93c74d91eb5c1c03a19597f6698d465
Segun Osiki <osiki.segun@gmail.com>
2023-01-19 13:49:13 +0100

    edit file

--- src/component/EmployeeList.jsx
+++ src/component/EmployeeList.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import EmployeeService from "./service/EmployeeService";
 
 const EmployeeList = () => {
 
     const navigate = useNavigate()
+    const [loading, setLoading] = useState(true);
+    const [employees, setEmployees] = useState(null);
+
+    useEffect(() => {
+     
+        const fetchData = async () => {
+            setLoading(true)
+            try{
+                const response = await EmployeeService.getEmployees();
+                setEmployees(response.data);
+            }catch(error){
+                console.log(error);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, [])
+    
     return ( 
         <div className="container mx-auto my-8">
             <div className="h-12">
@@ -21,16 +41,18 @@ const EmployeeList = () => {
                             <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">Actions</th>
                         </tr>
                     </thead>
+                    {!loading && (
                     <tbody className="bg-white">
-                        <tr>
+                        {employees.map((employee) => (
+                        <tr key={employee.id}>
                             <td className="text-left px-6 py-4 whitespace-nowrap">
-                                <div className="text-small text-gray-500">Jon</div>
+                                <div className="text-small text-gray-500">{employee.firstName}</div>
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap">
-                                <div className="text-small text-gray-500">Snow</div>
+                                <div className="text-small text-gray-500">{employee.lastName}</div>
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap font-medium text-small">
-                                <div className="text-small text-gray-500">Jon@gmail.com</div>
+                                <div className="text-small text-gray-500">{employee.email}</div>
                             </td>
                             <td className="text-right px-6 py-4 whitespace-nowrap">
                                 <a href="http://" 
@@ -39,7 +61,8 @@ const EmployeeList = () => {
                                 className="text-indigo-600 hover:text-indigo-800">Delete</a>
                             </td>
                         </tr>
-                    </tbody>
+                        ))}
+                    </tbody>)}
                 </table>
             </div>
         </div>

97c1803284f537f40ce9531430f20b88dc3cb21f
Segun Osiki <osiki.segun@gmail.com>
2023-01-19 08:54:51 +0100

    add paths to route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddEmployee from './component/AddEmployee';
 import NavBar from './component/NavBar.jsx';
+import EmployeeList from './component/EmployeeList.jsx';
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
       <NavBar />
-      <AddEmployee />
+      <Routes>
+        <Route index element={<EmployeeList />} />
+        <Route path='/' element={<EmployeeList />} />
+        <Route path='/employeeList' element={<EmployeeList />} />
+        <Route path='/addEmployee' element={<AddEmployee />} />
+
+      </Routes>
+      </BrowserRouter>
+      
+      
     </div>
   );
 }
